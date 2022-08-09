2bb7cfd7a8cd6d0867485c5dd62d86bbdc1e63b7
magbrnAR <88108801+magbrnAR@users.noreply.github.com>
2022-08-09 23:50:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,20 +8,69 @@
     
     <!-- <title>Simple Recorder.js demo with record, stop and pause - addpipe.com</title> -->
 </head>
+<style>
+    #myVideo {
+        width: 100vw;
+  height: 100vh;
+  object-fit: cover;
+  position: fixed;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  z-index: -1;
+}
 
+#speechTranscriptContainer {
+  border-style:inset;
+  color:#ffffff;
+  background: rgba(0, 0, 0, 0.5);
+    width: 70%;
+    height:70%;
+}
+/* #aiText {
+  border-style:outset;
+  color:#ffffff;
+  background: rgba(0, 0, 0, 0.5);
+    width: 70%;
+    height:70%;
+    
+} */
+.content{
+  
+  background: rgba(0, 0, 0, 0.5);
+  color: #ffffff;
+}
+
+.myBtn {
+  border: none;
+  background: #2e3c53;
+  color: #fff;
+  cursor: pointer;
+}
+
+.myBtn:hover {
+  background: #ddd;
+  color: black;
+}
+</style>
 <body>
+
+<div class = "content">
     <center>
     <br><br><br><br><br><br>
     <!-- <h1>Simple Recorder.js demo</h1> -->
     <div id="controls">
-        <button id="recordButton">Record</button>
+        <input class="content" id="inputtext" type="text">
+        <button class="myBtn" id="myvoice" >speack</button><br><br><br>
+        <button class="myBtn" id="recordButton">Record</button>
         <!-- <button id="pauseButton" disabled>Pause</button> -->
         <button id="stopButton" disabled>Stop</button>
 
         <!-- <button type="button" id='myvoice'>Listen to me</button> -->
 
     </div>
-
+    <br><br><br>
     <!-- <div id="formats">Format: start recording to see sample rate</div> -->
     <!-- <ol id="recordingsList"></ol> -->
     <!-- &lt;!&ndash; inserting these scripts at the end to be able to use all the elements in the DOM &ndash;&gt; -->
@@ -29,11 +78,19 @@
     <script src="styles/js/app.js"></script>
 <!--     {% if transcript != "" %} -->
     <div id="speechTranscriptContainer">
-        <h1>Results</h1>
+        <!-- <h1>Results</h1> -->
         <p id="speechText"></p>
+        <br><br>
         <p id="aiText"></p>
+
+        
     </div>
 </center>
+</div>
+<video autoplay muted loop id="myVideo">
+  <source src="video_1.mp4" type="video/mp4">
+</video>
+
 <!--     {% endif %} -->
 </body>
 
